2f68ac5422b93d0b88880044e66e3647dfa9fcb6
root <root@mitsuhiro-Virtual-Machine.(none)>
2014-04-30 12:15:35 +1000

    CVJ-173 notification to custodian or creator

--- ckanext/review/commands/notify.py
+++ ckanext/review/commands/notify.py
@@ -5,7 +5,7 @@ import ckan
 import pylons
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as tk
-import ckan.logic.converters as converters
+from ckan.logic import get_action, NotFound
 import datetime
 import ckanext.review.helpers as h
 from ckan.logic.validators import object_id_validators
@@ -41,17 +41,31 @@ class NotifyCommand(CkanCommand):
         #get all packages that need to be reviewed
         package_reviews = get_by_daterange(ckan.model.Session, datetime.date(2001,1,1), datetime.date.today())
 
-        review_context = {
-            'model': ckan.model,
-            'user':admin_user["name"],
-            'session': ckan.model.Session,
-            'ignore_auth': True
-        }
         for pr in package_reviews:
             #get the package
             pkg = ckan.model.Package.get(pr.package_id)
             pkg_dict = ckan.lib.dictization.table_dictize(pkg, context)
-            
-            h.create_review_activity(review_context, pkg_dict, h.dataset_activity_type_review_package)
+            if 'maintainer' in pkg_dict:
+                recipient = pkg_dict.get('maintainer')
+            elif 'creator_user_id' in pkg_dict:
+                recipient = pkg_dict.get('creator_user_id')
+            else:
+                raise ValueError('User not found')
+
+            # Check if user exists
+            try:
+                user = ckan.model.User.get(recipient)
+
+                review_context = {
+                    'model': ckan.model,
+                    'user':user.name,
+                    'session': ckan.model.Session,
+                    'ignore_auth': True
+                }
+
+                h.create_review_activity(review_context, pkg_dict, h.dataset_activity_type_review_package)
+            except NotFound:
+#                raise ValueError('User not found')
+                print 'User not found %s '%(recipient)
         print 'done'
         

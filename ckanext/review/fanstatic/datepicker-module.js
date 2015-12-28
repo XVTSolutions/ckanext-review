c820edb6930b263390eb442dde69bedd2b3655df
ozakimitsuhiro <mitsuhiro@xvt.com.au>
2015-12-29 08:43:12 +1100

    CVJ-241 readonly datepicker

--- ckanext/review/fanstatic/datepicker-module.js
+++ ckanext/review/fanstatic/datepicker-module.js
@@ -1,6 +1,10 @@
 //loads jquery-ui's datepicker for the input field
 $(function() {
-
-    $("#field-next_review_date").datepicker({ dateFormat: "dd-mm-yy" });
-
+	$.each($(".vdoj-datepicker input:text"), function(){
+	    $(this).datepicker({ dateFormat: "dd-mm-yy" });
+	    if ($(this).attr('readonly')=='readonly'){
+			$(this).datepicker("option", "minDate", -1);
+			$(this).datepicker("option", "maxDate", -2);
+	    }
+	});
 });
